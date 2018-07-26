7dac7ef9e31623cc2051ed10e1882378d920be51
Akhil <akhilkumar.nair@nike.com>
2018-07-26 14:35:17 -0700

    JSON-stringify example

---
+++ json-stringify-ex.js
@@ -0,0 +1,16 @@
+let room = {
+  number: 23
+};
+
+let meetup = {
+  title: "Conference",
+  participants: [{name: "John"}, {name: "Alice"}],
+  place: room // meetup references room
+};
+
+room.occupiedBy = meetup; // room references meetup
+
+alert( JSON.stringify(meetup, function replacer(key, value) {
+  alert(`${key}: ${value}`); // to see what replacer gets
+  return (key == 'occupiedBy') ? undefined : value;
+}));
